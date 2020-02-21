00b4a252613191f9706f4123840c24398ae918e7
naisuu <juliuscathalina96@gmail.com>
2020-02-21 19:52:51 +0100

    add todo's and privatize global vars

---
+++ README.md
@@ -0,0 +1,4 @@
+### TODO LIST:
+
+-[.] Implement a smart heuristic for the eval function
+-[.] Implement alpha-beta pruning and replace the naive minimax
--- minimax.py
+++ minimax.py
@@ -3,11 +3,11 @@ from hex_skeleton import HexBoard
 import numpy as np
 
 # global vars
-N: int = 3
-INF: float = 99999.0
+_board_size: int = 3
+_INF: float = 99999.0
 
 # initialize board with size n
-board = HexBoard(N)
+_board = HexBoard(_board_size)
 
 
 def _update_board(board: HexBoard, l_move, is_max: bool) -> HexBoard:
@@ -28,14 +28,14 @@ def minimax(board: HexBoard, depth: int, is_max: bool) -> float:
     if legals:
 
         if is_max:
-            g: float = -INF
+            g: float = -_INF
 
             for move in legals:
                 updated_board: HexBoard = _update_board(board, move, is_max)
                 g = max(g, minimax(updated_board, depth-1, not is_max))
 
         else:
-            g: float = INF
+            g: float = _INF
 
             for move in legals:
                 updated_board: HexBoard = _update_board(board, move, is_max)
@@ -44,5 +44,5 @@ def minimax(board: HexBoard, depth: int, is_max: bool) -> float:
         return g
 
 
-final = minimax(board, 4, True)
+final = minimax(_board, 4, True)
 print(final)
