400f0ce709784736522b5067e605358699bece7a
Naisuu <julius.cathalina@gmail.com>
2020-02-25 20:10:05 +0100

    add a simpler implementation of dijkstra, needs fixing

--- minimax.py
+++ minimax.py
@@ -1,11 +1,6 @@
 from hex_skeleton import HexBoard
-from graph import Graph
 
 import numpy as np
-
-# probably have to do this better later
-from hexagon import Hexagon
-# Remove when refactoring
 
 # global vars
 _board_size: int = 3
@@ -21,19 +16,44 @@ def _update_board(board: HexBoard, l_move, is_max: bool) -> HexBoard:
 
 def dummy_eval() -> float:
     return np.random.randint(0, 10)
+    
+def simple_dijkstra(board: HexBoard, source, is_max):
+    
+    Q = set()
+    V_set = board.get_all_vertices()
+    dist = {}
+    prev = {}
+    for v in V_set:
+        dist[v] = np.inf
+        prev[v] = None
+        Q.add(v)
+    dist[source] = 0
 
-def dijkstra_eval(board: HexBoard, is_max: bool):
-    # TODO finish implementing dijkstra in graph class
-    color = board.BLUE if is_max else board.RED
-    graph = Graph(board)
-    from_hex = Hexagon((0,1), board)
-    return graph.dijkstra(from_hex, is_max)
+    while len(Q) != 0:
+        u = min(dist, key=dist.get)
+        # print(f"U IS: {u}")
+        Q.remove(u)
+
+        neighbors = board.get_neighbors(u)
+        color = board.BLUE if is_max else board.RED
+        
+        for v in neighbors:
+            if v in Q: # Only check neighbours that are also in "Q"
+                len_u_v = 1 if board.is_color(v, color) else 0 # this isn't working as intended i think...
+                alt = dist[u] + len_u_v
+                if alt < dist[v]:
+                    dist[v] = alt
+                    prev[v] = u
+        dist.pop(u) # This is to ensure that the keys in "dist" match the ones in "Q"
+    
+    return dist, prev
 
 def minimax(board: HexBoard, depth: int, is_max: bool) -> float:
 
     if depth == 0 or board.is_game_over():
         # return dummy_eval()
-        return dijkstra_eval(board, is_max)
+        # return dijkstra_eval(board, is_max)
+        return simple_dijkstra(board, (0, 1), is_max)
 
     legals = board.get_move_list()
     if legals:
@@ -55,5 +75,13 @@ def minimax(board: HexBoard, depth: int, is_max: bool) -> float:
         return g
 
 
-final = minimax(_board, 4, True)
-print(final)
+# final = minimax(_board, 4, True)
+# print(final)
+
+hb = HexBoard(3)
+hb.place((1,1), hb.BLUE)
+hb.print()
+
+dist, prev = simple_dijkstra(hb, (1,1), False)
+print(dist)
+
